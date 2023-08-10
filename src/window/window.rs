4e5be32f4c43a5f2af30f6ee7c54f524a6332065
Morior <jordi.grados@escapadarural.com>
2023-08-10 11:12:13 +0200

    Remove public constants

--- src/window/window.rs
+++ src/window/window.rs
@@ -3,9 +3,9 @@ use ggez::conf::{WindowMode, WindowSetup};
 pub struct Window;
 
 impl Window {
-    pub const WIDTH: f32 = 1920.0;
-    pub const HEIGHT: f32 = 1080.0;
-    pub const TITLE: &str = "Beat 'em up";
+    const WIDTH: f32 = 1920.0;
+    const HEIGHT: f32 = 1080.0;
+    const TITLE: &str = "Beat 'em up";
 
     pub fn create_window_setup() -> WindowSetup {
         WindowSetup::default().title(Self::TITLE)
