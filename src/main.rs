d5108b9dd2b1ec7c386cd466aac99006d48f2b73
Morior <jordi.grados@escapadarural.com>
2023-08-10 10:25:22 +0200

    Simplify main file

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,29 @@
-//! The simplest possible example that does something.
-#![allow(clippy::unnecessary_wraps)]
-
 use ggez::{
     event,
-    glam::*,
-    graphics::{self, Color},
-    Context, GameResult,
+    graphics::{self},
+    Context, ContextBuilder, GameResult,
 };
+use graphics::{Canvas, Color};
+use ggez::GameError;
+use event::EventHandler;
 
 struct MainState {
-    pos_x: f32,
-    circle: graphics::Mesh,
 }
 
 impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            vec2(0., 0.),
-            100.0,
-            2.0,
-            Color::WHITE,
-        )?;
-
-        Ok(MainState { pos_x: 0.0, circle })
+    fn new(_ctx: &mut Context) -> GameResult<MainState> {
+        Ok(MainState { })
     }
 }
 
-impl event::EventHandler<ggez::GameError> for MainState {
+impl EventHandler<GameError> for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas =
-            graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
-
-        canvas.draw(&self.circle, Vec2::new(self.pos_x, 380.0));
+        let clear: Color = Color::from([0.1, 0.2, 0.3, 1.0]);
+        let canvas: Canvas = Canvas::from_frame(ctx, clear);
 
         canvas.finish(ctx)?;
 
@@ -47,7 +32,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
 }
 
 pub fn main() -> GameResult {
-    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
+    let cb = ContextBuilder::new("super_simple", "ggez");
     let (mut ctx, event_loop) = cb.build()?;
     let state = MainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
