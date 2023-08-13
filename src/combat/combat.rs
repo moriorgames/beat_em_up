495154c9c78e49857ccdff5ba2e97192c1dfb8a9
Morior <jordi.grados@escapadarural.com>
2023-08-13 22:17:40 +0200

    Move logic to move to move handlers to have more modularity on code

--- src/combat/combat.rs
+++ src/combat/combat.rs
@@ -1,29 +1,24 @@
 pub mod combat {
-    use crate::combat::{action::Action, direction::Direction, event::Event};
+    use crate::combat::{
+        action::Action,
+        event::Event,
+        move_handlers::move_handlers::{down, left, right, up},
+    };
     use std::collections::VecDeque;
 
+    const EVENT_HANDLERS: [fn(&Event) -> Option<Action>; 4] = [left, right, up, down];
+
     pub fn process_combat_queue(events: VecDeque<Event>) -> Vec<Action> {
-        events
-            .iter()
-            .filter_map(|event: &Event| match event {
-                Event::MoveLeft { id } => Some(Action::MoveEntity {
-                    id: *id,
-                    direction: Direction::Left,
-                }),
-                Event::MoveRight { id } => Some(Action::MoveEntity {
-                    id: *id,
-                    direction: Direction::Right,
-                }),
-                Event::MoveUp { id } => Some(Action::MoveEntity {
-                    id: *id,
-                    direction: Direction::Up,
-                }),
-                Event::MoveDown { id } => Some(Action::MoveEntity {
-                    id: *id,
-                    direction: Direction::Down,
-                }),
-                _ => None,
-            })
-            .collect()
+        let mut actions: Vec<Action> = Vec::new();
+
+        for event in &events {
+            for handler in &EVENT_HANDLERS {
+                if let Some(action) = handler(event) {
+                    actions.push(action);
+                }
+            }
+        }
+
+        actions
     }
 }
--- src/combat/mod.rs
+++ src/combat/mod.rs
@@ -3,3 +3,4 @@ pub mod combat;
 pub mod direction;
 pub mod event;
 pub mod event_queue;
+pub mod move_handlers;
