a0a0ede1955e1e35be4160d06290e46a360fa84d
Morior <jordi.grados@escapadarural.com>
2023-08-18 14:34:55 +0200

    change health bars

--- src/character/character_view.rs
+++ src/character/character_view.rs
@@ -10,7 +10,8 @@ pub mod character_view {
     use ggez::mint::Point2;
     use ggez::{Context, GameResult};
 
-    const HEALTH_BAR_HEIGHT: f32 = 6.0;
+    const HEALTH_BAR_HEIGHT: f32 = 8.0;
+    const HEALTH_BAR_FACTOR: f32 = 100.0;
     const HITBOX_DEBUG: bool = false;
 
     pub fn draw_characters(
@@ -85,15 +86,25 @@ pub mod character_view {
     }
 
     fn draw_bars(gfx: &mut Context, canvas: &mut Canvas, character: &Character) {
-        let x: f32 = character.position.x - character.size.w / 2.0;
+        let x: f32 = character.position.x - (character.size.w - (HEALTH_BAR_FACTOR)) / 2.0;
+        let y: f32 = character.position.y - character.size.h / 2.0 - HEALTH_BAR_HEIGHT;
+        let position: Position = Position::new(x, y);
+
+        let w: f32 = character.size.w - (HEALTH_BAR_FACTOR);
+        let h: f32 = HEALTH_BAR_HEIGHT;
+        let size: Size = Size::new(w, h);
+        let color: Color = Color::RED;
+        draw_solid_rectangle(gfx, canvas, &position, &size, color);
+
+        let x: f32 = character.position.x - (character.size.w - (HEALTH_BAR_FACTOR)) / 2.0;
         let y: f32 = character.position.y - character.size.h / 2.0 - HEALTH_BAR_HEIGHT;
         let position: Position = Position::new(x, y);
 
         let health_percentage: f32 = character.current_health as f32 / character.max_health as f32;
-        let w: f32 = character.size.w * health_percentage;
+        let w: f32 = character.size.w * health_percentage - (HEALTH_BAR_FACTOR);
         let h: f32 = HEALTH_BAR_HEIGHT;
         let size: Size = Size::new(w, h);
-        let color: Color = Color::RED;
+        let color: Color = Color::YELLOW;
         draw_solid_rectangle(gfx, canvas, &position, &size, color);
     }
 }
