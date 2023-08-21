9900df7b43d0e32d4f88ad6446898cb11147bf6f
Morior <jordi.grados@escapadarural.com>
2023-08-21 18:40:02 +0200

    Added attack behaviour to enemies

--- src/enemy/enemy_behaviour.rs
+++ src/enemy/enemy_behaviour.rs
@@ -32,28 +32,37 @@ pub mod enemy_behavior {
         let dir_x: f32 = player_position.x - enemy_position.x;
         let dir_y: f32 = player_position.y - enemy_position.y;
 
-        let magnitude: f32 = (dir_x * dir_x + dir_y * dir_y).sqrt();
-        let normalized_dir_x: f32 = dir_x / magnitude;
-        let normalized_dir_y: f32 = dir_y / magnitude;
-
-        let id: Uuid = character.id;
-        let direction = match (normalized_dir_x, normalized_dir_y) {
-            (x, y) if x > 0.0 && y > 0.0 => Direction::DownRight,
-            (x, y) if x > 0.0 && y < 0.0 => Direction::UpRight,
-            (x, y) if x < 0.0 && y > 0.0 => Direction::DownLeft,
-            (x, y) if x < 0.0 && y < 0.0 => Direction::UpLeft,
-            (x, _) if x > 0.0 => Direction::Right,
-            (x, _) if x < 0.0 => Direction::Left,
-            (_, y) if y > 0.0 => Direction::Down,
-            (_, y) if y < 0.0 => Direction::Up,
-            _ => return,
-        };
-
-        actions.push(Action::Moving {
-            id,
-            direction,
-            from: turn + 1,
-            to: turn + 20,
-        });
+        let distance: f32 = (dir_x.powi(2) + dir_y.powi(2)).sqrt();
+        if distance <= 110.0 {
+            actions.push(Action::Attacking {
+                id: character.id,
+                from: turn + 1,
+                to: turn + 22,
+            });
+        } else {
+            let magnitude: f32 = (dir_x * dir_x + dir_y * dir_y).sqrt();
+            let normalized_dir_x: f32 = dir_x / magnitude;
+            let normalized_dir_y: f32 = dir_y / magnitude;
+    
+            let id: Uuid = character.id;
+            let direction = match (normalized_dir_x, normalized_dir_y) {
+                (x, y) if x > 0.0 && y > 0.0 => Direction::DownRight,
+                (x, y) if x > 0.0 && y < 0.0 => Direction::UpRight,
+                (x, y) if x < 0.0 && y > 0.0 => Direction::DownLeft,
+                (x, y) if x < 0.0 && y < 0.0 => Direction::UpLeft,
+                (x, _) if x > 0.0 => Direction::Right,
+                (x, _) if x < 0.0 => Direction::Left,
+                (_, y) if y > 0.0 => Direction::Down,
+                (_, y) if y < 0.0 => Direction::Up,
+                _ => return,
+            };
+    
+            actions.push(Action::Moving {
+                id,
+                direction,
+                from: turn + 1,
+                to: turn + 20,
+            });
+        }
     }
 }
