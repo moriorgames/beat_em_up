cc5c5f71ff8859cd57b6bd6d0cadb257011ffd2f
Morior <jordi.grados@escapadarural.com>
2023-08-25 08:18:10 +0200

    Refactor on naming

---
+++ src/game/game_draw_in_combat.rs
@@ -0,0 +1,21 @@
+use crate::{
+    character::character_view::character_view::draw_characters,
+    world::world_view::character_view::draw, MainState,
+};
+use ggez::{graphics::Canvas, Context};
+
+pub fn execute(ctx: &mut Context, canvas: &mut Canvas, main_state: &mut MainState) {
+    let _ = draw(
+        ctx,
+        canvas,
+        &main_state.world,
+        &main_state.sprite_repository,
+    );
+
+    let _ = draw_characters(
+        ctx,
+        canvas,
+        &main_state.characters,
+        &main_state.sprite_repository,
+    );
+}
